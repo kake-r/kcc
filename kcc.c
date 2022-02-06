3e340f8838f2bf1182b390ee54ff0d80d39434c5
kake-r <kake.ren.003.009@outlook.jp>
2022-02-06 15:49:40 +0900

    四則演算のできる言語の作成（と再帰下降構文解析）

--- kcc.c
+++ kcc.c
@@ -13,6 +13,26 @@ typedef enum {
 	TK_EOF, //入力終了のトークン
 } TokenKind;
 
+//抽象構文木のノードの種類
+typedef enum {
+    ND_ADD, // +
+    ND_SUB, // -
+    ND_MUL, // *
+    ND_DIV, // /
+    ND_NUM, //整数
+} NodeKind;
+
+typedef struct Node Node;
+
+// 抽象構文木のノードの形
+struct Node {
+    NodeKind kind; //ノードの型 NUMなのか符号なのか
+    Node *lhs; //左辺 子ノード
+    Node *rhs; // 右辺 子ノード
+    int val; // 数字のノードのときにつかう
+};
+
+
 typedef struct Token Token;
 
 //トークン型
@@ -117,7 +137,7 @@ Token *tokenize() {
             p++;
             continue;
         }
-        if (*p == '+' || *p == '-') {
+        if (strchr("+-*/()", *p)) {
             cur = new_token(TK_RESERVED, cur, p++);
             //cur->val = strtol(p, &p, 10);
             continue;
@@ -136,6 +156,109 @@ Token *tokenize() {
     return head.next;
 }
 
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+    Node *node = calloc(1, sizeof(Node));
+    //メモリ確保
+    node->kind = kind;
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+
+//数字が入ってる末端のノード
+Node *new_node_num(int val) {
+    Node *node = calloc(1, sizeof(Node));
+    node-> kind = ND_NUM;
+    node-> val = val;
+    return node;
+}
+
+//四則演算の文法
+Node *expr();
+Node *mul();
+Node *primary();
+
+
+// primary = num | "(" expr ")"
+Node *primary() {
+    //トークンが（）
+    if (consume('(')) {
+        Node *node = expr();
+        expect(')');
+        return node;    
+    }
+    
+    //トークンが数字
+    return new_node_num(expect_number());
+
+}
+
+// mul     = primary ("*" primary | "/" primary)*
+Node *mul() {
+    Node *node = primary();
+
+    for (;;) {
+        if (consume('*'))
+            node = new_node(ND_MUL, node, primary());
+        else if (consume('/'))
+            node = new_node(ND_DIV, node, primary());
+        else 
+            return node;
+    }
+}
+
+
+// expr    = mul ("+" mul | "-" mul)*
+Node *expr() {
+    Node *node = mul();
+
+    for (;;) {
+        if (consume('+'))
+            node = new_node(ND_ADD, node, mul());
+        else if (consume('-'))
+            node = new_node(ND_SUB, node, mul());
+        else 
+            return node;
+    }
+}
+
+
+
+
+
+void gen(Node *node) {
+    if (node->kind == ND_NUM) {
+        printf("  push %d\n", node->val);
+        return;
+    }
+
+    gen(node->lhs);
+    gen(node->rhs);
+
+    printf("  pop rdi\n");
+    printf("  pop rax\n");
+
+    switch (node->kind) {
+    case ND_ADD:
+        printf("  add rax, rdi\n");
+        break;
+    case ND_SUB:
+        printf("  sub rax, rdi\n");
+        break;
+    case ND_MUL:
+        printf("  imul rax, rdi\n");
+        break;
+    case ND_DIV:
+        printf("  cqo\n");
+        printf("  idiv rdi\n");
+        break;
+    }
+
+    printf("  push rax\n");
+}
+
+
 
 //command line arguments ,count/vecter
 int main(int argc, char **argv) {
@@ -149,31 +272,21 @@ int main(int argc, char **argv) {
     //トークナイズする
     //改良版
     user_input = argv[1];
-    token = tokenize();
+    //token = tokenize(); どっちだ？
+    token = tokenize(user_input);
+    Node *node = expr();
 
     //アセンブリの前半
     printf(".intel_syntax noprefix\n");
     printf(".globl main\n");
     printf("main:\n");
-    
-    //式の最初が数字だと確認して mov命令を出力
-    printf("  mov rax, %d\n", expect_number());
-    
-    while (!at_eof()) {
-        if (consume('+')) {
-            printf("  add rax, %d\n", expect_number());
-            continue;
-        }
-        // if (consume('-')) {
-        //     printf("  add sub, %d\n", expect_number());
-        //     continue;
-        // }
-        expect('-');
-        printf("  sub rax, %d\n", expect_number());
-        
 
-    }
-
+    //抽象構文木を下りながらコードを生成
+    gen(node);
+    
+    // スタックのトップが式全体の値になる
+    // それをRAXにロードして関数からの返り値とする
+    printf("  pop rax\n");
     printf("  ret\n");
     return 0;
 }
